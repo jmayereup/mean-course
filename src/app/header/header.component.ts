b9fb44e19fa39bd7caaecb37193294a4cb1e2445
Jacob M <jmayereup@gmail.com>
2018-08-25 20:59:55 +0700

    Finished lesson 19 - Adding a Toolbar

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './header.component.html',
+  selector: 'app-header-component'
+})
+
+export class HeaderComponent {
+
+}
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './post-create.component.html',
-  selector: 'app-post-create'
+  selector: 'app-post-create',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
   enteredValue = '';
